b37205f0a657f387b98c1bf684ff21c338cc38bd
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-04 21:44:56 -0800

    added current time to bottom of jumbotron and dynamically changed input field backgrounds based on current time

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 renderScores();
+time();
+
 
 $(".save").on("click",function(){
+
 //key-val
 var dataIndex = $(this).parent().attr("data-index")
-//console.log($(this).parent().attr("data-index")); 
 
 //val-item
 var value = $(this).siblings(".text-field").children().children().val();
-//console.log($(this).siblings(".text-field").children().children().val());
 
 localStorage.setItem(dataIndex, value);
+
 });
+
+function time()
+{
+    var now = moment().format("dddd") + ", " + moment().format("MMMM") + " " + moment().format("do") ;
+    var hour = moment().hour();
+    var dataArr = $(".row");
+    $("#currentDay").text(now);
+    
+    for(var i = 0; i<9; i++)
+    {
+        var dataTime = $(dataArr[i]).attr("data-time");
+        var inputField = $(dataArr[i]).find("input");
+        console.log(inputField);
+
+
+        if(parseInt(dataTime) > hour)
+        {
+           
+            inputField.attr("class", "form-control text-white bg-success");
+           // console.log("green");
+        }
+        if (parseInt(dataTime) === hour)
+        {
+            inputField.attr("class", "form-control text-white bg-danger");
+            //console.log("red");
+        }
+        if(parseInt(dataTime) < hour)
+        {
+            inputField.attr("class", "form-control text-white bg-secondary");
+           // console.log("grey");
+           
+
+        }
+
+        console.log(dataTime);
+
+
+
+    }
+    
+
+}
 
 
 
