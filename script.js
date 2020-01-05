c4a9136b7e366eb34db1093551f1ee194cd70fd5
FrantzCFelix <Frantz.C.Felix@gmail.com>
2020-01-04 19:11:42 -0800

    saved notes to localStorage and render the notes from localStorage on page load

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-
+renderScores();
 
 $(".save").on("click",function(){
 //key-val
-console.log($(this).parent().attr("data-index")); 
+var dataIndex = $(this).parent().attr("data-index")
+//console.log($(this).parent().attr("data-index")); 
 
 //val-item
-console.log($(this).siblings(".text-field").children().children().val());
+var value = $(this).siblings(".text-field").children().children().val();
+//console.log($(this).siblings(".text-field").children().children().val());
 
-
+localStorage.setItem(dataIndex, value);
 });
+
+
+
+function renderScores()
+{   var dataArr = $(".row");
+    for(var i = 0; i<9; i++)
+    {
+        var dataIndex = $(dataArr[i]).attr("data-index");
+        var val = localStorage.getItem(dataIndex); 
+        $(dataArr[i]).children(".text-field").find("input").val(val);
+        //console.log($(dataArr[i]).val(val));
+      //  $(dataArr[i]).val(val);
+
+        
+       
+    }
+
+   localStorage.getItem(dataIndex);  
+
+}
